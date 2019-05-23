ca011d00a8775ad4fb7ee9e555b2b408b20e4557
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-23 18:57:55 +0200

    Create bestuurseeinheid selector component

---
+++ app/components/select-bestuurseenheid.js
@@ -0,0 +1,49 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { task, timeout } from 'ember-concurrency';
+
+export default Component.extend({
+  store: service(),
+
+  async init() {
+    this._super(...arguments);
+    const options = this.store.query('bestuurseenheid', {
+      sort: 'naam',
+      include: ['classificatie']
+    });
+    this.set('options', options);
+  },
+
+  async didReceiveAttrs() {
+    this._super(...arguments);
+    if (this.value && !this.selected) {
+      const bestuurseenheids = this.store.query('bestuurseenheid', {
+        filter: { id: this.value },
+        page: { size: this.value.split(",").length}
+      });
+      this.set('selected', bestuurseenheids);
+    } else if (!this.value) {
+      this.set('selected', null);
+    }
+  },
+
+  selected: null,
+  value: null, // id of selected record
+  onSelectionChange: null,
+
+  search: task(function* (term) {
+    yield timeout(600);
+    return this.store.query('bestuurseenheid', {
+      sort: 'naam',
+      include: ['classificatie'],
+      filter: term
+    });
+  }),
+
+  actions: {
+    changeSelected(selected) {
+      this.set('selected', selected);
+      this.onSelectionChange(selected);
+    }
+  }
+});
