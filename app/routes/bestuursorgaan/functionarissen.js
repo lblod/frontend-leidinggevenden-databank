7d97a3791185dfd793be5fbb0c921fca71014ecf
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-24 11:37:14 +0200

    Add DataTableRouteMixin to the route

--- app/routes/bestuursorgaan/functionarissen.js
+++ app/routes/bestuursorgaan/functionarissen.js
@@ -1,7 +1,5 @@
 import Route from '@ember/routing/route';
+import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
-export default Route.extend({
-  model(params){
-    
-  }
+export default Route.extend(DataTableRouteMixin, {
 });
