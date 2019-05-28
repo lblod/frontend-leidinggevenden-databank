d69f505a3d3ec39e28436ac1b604283852c2f845
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-28 14:27:01 +0200

    Expose the bestuurseenheid to route and controller

--- app/routes/bestuursorgaan/functionarissen.js
+++ app/routes/bestuursorgaan/functionarissen.js
@@ -2,4 +2,16 @@ import Route from '@ember/routing/route';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
 export default Route.extend(DataTableRouteMixin, {
+  modelName: 'functionaris',
+  bestuurseenheid: null,
+  async mergeQueryOptions(params) {
+    this.set('bestuurseenheid', await this.store.findRecord('bestuurseenheid', params.bestuureenheid_id));
+    return {
+    };
+  },
+
+  async setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('bestuurseenheid', this.bestuurseenheid);
+  }
 });
