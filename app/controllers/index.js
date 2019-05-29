be07c4d9d1b676372fbeb6dca516f84bd23bbd98
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-29 18:07:54 +0200

    Avoid null pointer error by providing a default
    If user clears the current selection, the onChange event is triggered while value is null. In such a case we want to clear the current bestuurseenheidId.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,7 +7,7 @@ export default Controller.extend({
   actions: {
     setBestuurseenheid(value) {
       this.set('bestuurseenheid', value);
-      this.set('bestuurseenheidId', value.id);
+      this.set('bestuurseenheidId', (value && value.id) || '');
     }
   }
 });
