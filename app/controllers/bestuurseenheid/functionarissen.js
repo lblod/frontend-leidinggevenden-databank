76d9f4d0f27049b4a9057b54344c9cc2c4e52484
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-06-05 18:03:15 +0200

    make sure defaultqueryparmamixin works

--- app/controllers/bestuurseenheid/functionarissen.js
+++ app/controllers/bestuurseenheid/functionarissen.js
@@ -1,11 +1,11 @@
 import Controller from '@ember/controller';
 import { computed }  from '@ember/object';
+import DefaultQueryParamsMixin from 'ember-data-table/mixins/default-query-params';
 
-export default Controller.extend({
+export default Controller.extend(DefaultQueryParamsMixin, {
   sort: 'is-bestuurlijke-alias-van.achternaam',
 
   filteredModel: computed('model', function() {
     return this.model.filter(functionaris => functionaris.isOngoing);
   })
 });
-
