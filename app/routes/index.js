8d04c13d32ec3f3bb0530cedd537eb5104aa8cbe
Mehran Naghizadeh <tarhineh@gmail.com>
2019-06-11 12:36:56 +0200

    Avoid model refresh on bestuurseenheidId change
    There is no need to make the model refresh on a change of the bestuurseenheidId query param since the model hook isn't doing anything.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   queryParams: {
-    bestuurseenheidId: { refreshModel: true }
+    bestuurseenheidId: { refreshModel: false }
   },
 
   model(params) {
