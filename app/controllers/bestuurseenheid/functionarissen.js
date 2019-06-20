81f25d2a0172969478d2195991af10c70031af70
Erika Pauwels <erika.pauwels@gmail.com>
2019-06-20 12:43:59 +0200

    Ensure all functionarissen are loaded by increasing page size
    Since we filter the functionarissen collection in the frontend,
    pagination is disabled. Therefore we need to make sure all
    functionarissen are loaded on one page.

--- app/controllers/bestuurseenheid/functionarissen.js
+++ app/controllers/bestuurseenheid/functionarissen.js
@@ -4,6 +4,7 @@ import DefaultQueryParamsMixin from 'ember-data-table/mixins/default-query-param
 
 export default Controller.extend(DefaultQueryParamsMixin, {
   sort: 'is-bestuurlijke-alias-van.achternaam',
+  size: 1000, // make sure to load enough functionarissen since we filter in the frontend
 
   filteredModel: computed('model', function() {
     return this.model.filter(functionaris => functionaris.isOngoing);
