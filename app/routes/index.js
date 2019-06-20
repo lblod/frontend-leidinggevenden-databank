49026f2d35e19693c07bfb9becfa8e8c20f537f0
Mehran Naghizadeh <tarhineh@gmail.com>
2019-06-20 08:56:07 +0200

    Replace " with '

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,7 +7,7 @@ export default Route.extend({
 
   async model(params) {
     if (params.bestuurseenheidId) {
-      this.controllerFor("index").set('bestuurseenheid', await this.store.findRecord('bestuurseenheid', params.bestuurseenheidId));
+      this.controllerFor('index').set('bestuurseenheid', await this.store.findRecord('bestuurseenheid', params.bestuurseenheidId));
     }
   }
 });
