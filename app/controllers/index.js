c1b856ecb93d240269039b3cd3e8a3855ccf8cb0
Erika Pauwels <erika.pauwels@gmail.com>
2019-06-20 12:20:22 +0200

    Replace button with link-to to navigate to another route on index page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,17 +2,11 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
-  router: service(),
-
   queryParams: ['bestuurseenheidId'],
 
   actions: {
     setBestuurseenheidId(value) {
       this.set('bestuurseenheidId', (value && value.id));
-    },
-
-    displayLeidinggevenden() {
-      this.router.transitionTo('bestuurseenheid.functionarissen', this.bestuurseenheidId);
     }
   }
 });
