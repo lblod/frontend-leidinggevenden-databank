0cf1586d3f0a6c4ea9fb59d5b7d009e610369f64
Erika Pauwels <erika.pauwels@gmail.com>
2019-06-20 12:19:13 +0200

    Remove unnecessary model hook in index route
    The route only defines a bestuurseenheidId query params. Loading the
    bestuurseenheid record based on the id is done by the
    select-bestuurseenheid component.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,5 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model(params) {
-    if (params.bestuurseenheidId) {
-      this.controllerFor('index').set('bestuurseenheid', this.store.findRecord('bestuurseenheid', params.bestuurseenheidId));
-    }
-  }
+
 });
