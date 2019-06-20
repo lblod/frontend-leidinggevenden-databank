3c2fae89178cfe5240cf382a749e69a1866bb981
Mehran Naghizadeh <tarhineh@gmail.com>
2019-06-20 10:38:18 +0200

    Get rid of the bestuurseenheid property
    bestuurseenheidId is enough for all our needs.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,13 +7,12 @@ export default Controller.extend({
   queryParams: ['bestuurseenheidId'],
 
   actions: {
-    setBestuurseenheid(value) {
-      this.set('bestuurseenheid', value);
+    setBestuurseenheidId(value) {
       this.set('bestuurseenheidId', (value && value.id));
     },
 
     displayLeidinggevenden() {
-      this.router.transitionTo('bestuurseenheid.functionarissen', this.bestuurseenheid.id);
+      this.router.transitionTo('bestuurseenheid.functionarissen', this.bestuurseenheidId);
     }
   }
 });
