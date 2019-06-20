60dc543411146d88df86ef8c495d2ad1958c225b
Mehran Naghizadeh <tarhineh@gmail.com>
2019-06-20 10:42:58 +0200

    Remove unnecessary async-await

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  async model(params) {
+  model(params) {
     if (params.bestuurseenheidId) {
-      this.controllerFor('index').set('bestuurseenheid', await this.store.findRecord('bestuurseenheid', params.bestuurseenheidId));
+      this.controllerFor('index').set('bestuurseenheid', this.store.findRecord('bestuurseenheid', params.bestuurseenheidId));
     }
   }
 });
