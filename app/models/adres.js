128be0cb5ad1ba73e88c708c7366844f1b4881bd
Erika Pauwels <erika.pauwels@gmail.com>
2019-07-18 22:13:51 +0200

    Fix RDFa annotations in functionaris table

--- app/models/adres.js
+++ app/models/adres.js
@@ -1,12 +1,8 @@
 import attr from 'ember-data/attr';
 import Model from 'ember-data/model';
-import { collect } from '@ember/object/computed';
 
 export default Model.extend({
-  // A string representation of this model, based on its attributes.
-  // This is what mu-cl-resources uses to search on, and how the model will be presented while editing relationships.
-  stringRep: collect.apply(this,['id', 'busnummer', 'huisnummer', 'straatnaam', 'postcode',  'gemeente', 'land', 'volledigAdres', 'adresRegisterId', 'adresRegisterUri']),
-
+  uri: attr(),
   busnummer: attr(),
   huisnummer: attr(),
   straatnaam: attr(),
@@ -27,6 +23,6 @@ export default Model.extend({
     land: "adres:land",
     volledigAdres : "locn:fullAddress",
     adresRegisterId: "lblodlg:adresRegisterId",
-    adresRegisterUri: "lblodlg:adresRegisterUri"
+    adresRegisterUri: "adres:verwijstNaar"
   })
 });
