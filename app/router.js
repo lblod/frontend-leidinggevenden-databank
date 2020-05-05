27c36e6bcb036d7aaf78864e35db9f986dad04a7
Oscar <lagartoverde97@gmail.com>
2020-05-05 17:27:09 +0200

    upgraded to ember 3.18

--- app/router.js
+++ app/router.js
@@ -1,10 +1,10 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
   this.route('bestuurseenheid', { path: "/bestuurseenheden/:bestuurseenheid_id" }, function() {
@@ -19,5 +19,3 @@ Router.map(function() {
     path: '/*wildcard'
   });
 });
-
-export default Router;
