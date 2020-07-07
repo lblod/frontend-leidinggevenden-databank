2ab43aa80b5dff2d2d0db8977cc776e0c33637dd
Oscar Rodriguez <oscar@verify.as>
2020-07-07 18:44:09 +0200

    converted the controllers

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,15 +1,15 @@
-import { reads } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 import ENV from 'frontend-leidinggevenden-databank/config/environment';
 
-export default Controller.extend({
-  fastboot: service(),
-  isFastBoot: reads('fastboot.isFastBoot'),
-
-  init() {
-    this._super(...arguments);
+export default class Application extends Controller {
+  @service fastboot;
+  constructor() {
+    super(...arguments);
     this.set('header', ENV['vo-webuniversum']['header']);
     this.set('footer', ENV['vo-webuniversum']['footer']);
   }
-});
+  get isFastBoot() {
+    return fastboot.isFastBoot;
+  }
+}
