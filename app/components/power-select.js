b410ff37717ce0f9e17fc7448a51748d1e8b3e57
Oscar Rodriguez <oscar@verify.as>
2020-08-06 11:25:07 +0200

    fixed errors related to the ember-power-select

--- app/components/power-select.js
+++ app/components/power-select.js
@@ -4,8 +4,6 @@ export class PowerSelect extends EmberPowerSelect {
   constructor() {
     super(...arguments);
     this.loadingMessage = 'Aan het laden...';
-    this.noMatchesMessage = 'Geen resultaten';
-    this.searchMessage = 'Typ om te zoeken';
   }
 }
 
