190647ae7d70afe4e0e6a0c7b0f1b0a90b448885
Dries Hugaerts <dries.hugaerts@live.be>
2020-12-07 13:52:04 +0100

    added back functionarissen filter in the controller + added meta.count to fix issue were the total wasn't visible on the view.

--- app/controllers/bestuurseenheid/functionarissen.js
+++ app/controllers/bestuurseenheid/functionarissen.js
@@ -5,6 +5,13 @@ import DefaultQueryParamsMixin from 'ember-data-table/mixins/default-query-param
 export default Controller.extend(DefaultQueryParamsMixin, {
   sort: 'is-bestuurlijke-alias-van.achternaam',
   page: 0,
-  size: 20,
+  size: 1000,
 
+  filteredModel: computed('model', function() {
+    const content = this.model.filter(functionaris => functionaris.isOngoing);
+    content.meta = {
+      count: content.length // NOTE: need to assign this to make the total visible
+    }
+    return content;
+  })
 });
