9ced80801840837e58300f7d3784aac707d4083d
achraf <achrafatauil@hotmail.com>
2020-12-18 13:15:13 +0100

    run ember-cli-update 3.20

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'frontend-leidinggevenden-databank/app';
+import config from 'frontend-leidinggevenden-databank/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
