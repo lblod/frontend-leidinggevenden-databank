10333368370d8b40b9004b6afc1c6a72005443da
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-09-22 11:25:19 +0200

    Allow users to load more search results (#8)
    The search results are paginated by the server. Before this change not all options would be displayed and there was no option to load more. Some users couldn't find the option they needed and making the search term more specific isn't always possible.
    
    This adds an extra button which users can click to manually load more search results.

--- app/components/select-bestuurseenheid.js
+++ app/components/select-bestuurseenheid.js
@@ -3,40 +3,58 @@ import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { timeout } from 'ember-concurrency';
-import { task, restartableTask } from 'ember-concurrency-decorators';
+import { dropTask, task, restartableTask } from 'ember-concurrency-decorators';
 
 export default class SelectBestuurseenheid extends Component {
   @service store;
   @tracked selected = null;
-  @tracked options;
+  @tracked exampleOptions = [];
+  @tracked searchData;
 
   constructor() {
     super(...arguments);
     this.loadData.perform();
   }
 
-  @task
-  * loadData() {
-    const options = yield this.store.query('bestuurseenheid', {
-      sort: 'naam',
-      include: 'classificatie'
-    });
-    this.options = options;
-    this.updateSelectedValue();
+  get isSearching() {
+    return Boolean(this.searchData);
   }
 
-  
-  value = null; // id of selected record
-  onSelectionChange = null;
+  get options() {
+    return this.isSearching ? this.searchData.results : this.exampleOptions;
+  }
+
+  @task
+  *loadData() {
+    this.exampleOptions = yield this.fetchBestuurseenheden();
+    this.updateSelectedValue();
+  }
 
   @restartableTask
   *search(term) {
     yield timeout(600);
-    return this.store.query('bestuurseenheid', {
-      sort: 'naam',
-      include: 'classificatie',
-      filter: term
+
+    let result = yield this.fetchBestuurseenheden({
+      filter: term,
     });
+
+    this.searchData = new SearchData({
+      totalResultAmount: result.meta.count,
+      searchTerm: term,
+      results: result.toArray(),
+    });
+  }
+
+  @dropTask
+  *loadMoreSearchResults() {
+    if (this.isSearching) {
+      let results = yield this.fetchBestuurseenheden({
+        filter: this.searchData.searchTerm,
+        "page[number]": ++this.searchData.currentPage,
+      });
+
+      this.searchData.addSearchResults(results.toArray());
+    }
   }
 
   @action
@@ -54,4 +72,38 @@ export default class SelectBestuurseenheid extends Component {
     this.args.onSelectionChange(selected);
   }
 
+  @action
+  registerAPI(api) {
+    if (!api.searchText && this.searchData) {
+      this.searchData = null;
+    }
+  }
+
+  async fetchBestuurseenheden(searchQuery = {}) {
+    return this.store.query("bestuurseenheid", {
+      sort: "naam",
+      include: "classificatie",
+      "page[number]": 0,
+      ...searchQuery,
+    });
+  }
+}
+
+class SearchData {
+  @tracked results = [];
+  currentPage = 0;
+
+  constructor({ totalResultAmount, searchTerm, results }) {
+    this.totalResultAmount = totalResultAmount;
+    this.searchTerm = searchTerm;
+    this.results = results;
+  }
+
+  get canLoadMoreSearchResults() {
+    return this.totalResultAmount > this.results.length;
+  }
+
+  addSearchResults(newResults = []) {
+    this.results = [...this.results, ...newResults];
+  }
 }
