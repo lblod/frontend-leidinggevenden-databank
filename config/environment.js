300a725970798b55bc15f7c02e3ab7bc974f4a62
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-01-06 12:48:37 +0100

    Update to the 3.28 LTS blueprint

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(environment) {
+module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'frontend-leidinggevenden-databank',
     environment,
@@ -13,8 +13,8 @@ module.exports = function(environment) {
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
+        Date: false,
+      },
     },
     fastboot: {
       hostWhitelist: [ "leidinggevenden.lokaalbestuur.vlaanderen.be",
