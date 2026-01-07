0f18184d07a86bd8864da1efb3c7c1bdb835ae31
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-01-07 15:03:28 +0100

    Simplify the inflector rules

---
+++ app/config/custom-inflector-rules.js
@@ -0,0 +1,9 @@
+import { irregular } from '@ember-data/request-utils/string';
+
+irregular('adres', 'adressen');
+irregular('bestuurseenheid', 'bestuurseenheden');
+irregular('bestuursfunctie', 'bestuursfuncties');
+irregular('bestuursorgaan', 'bestuursorganen');
+irregular('contact-punt', 'contact-punten');
+irregular('functionaris', 'functionarissen');
+irregular('persoon', 'personen');
